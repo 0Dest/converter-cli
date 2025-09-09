ea640e50ce7b0709b1bef71f233693c3d4a17084
Dest0 <dest.supp@proton.me>
2025-09-09 17:46:10 +0300

    feat: add weight units support

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 4 {
-        println!("More arguments pls");
+        println!("Please provide 3 arguments: <number> <from-unit> <to-unit>");
         return;
     }
 
@@ -19,9 +19,12 @@ fn main() {
 
 fn convert(number: &f64, from: &str, to: &str) {
     let length_list = ["mm", "cm", "in", "dm", "ft", "yd", "m", "km", "mi"];
+    let weight_list = ["mg", "g", "kg", "t", "oz", "lb", "st", "cwt"];
 
     if length_list.iter().any(|&x| x == from) && length_list.iter().any(|&x| x == to) {
         length(number, from, to);
+    } else if weight_list.iter().any(|&x| x == from) && weight_list.iter().any(|&x| x == to) {
+        weight(number, from, to);
     } else {
         println!("Unknown unit")
     }
@@ -38,6 +41,23 @@ fn length(number: &f64, from: &str, to: &str) {
     length_units.insert("m", 1.0);
     length_units.insert("km", 1000.0);
     length_units.insert("mi", 1609.344);
+
+    if let (Some(&from_factor), Some(&to_factor)) = (length_units.get(from), length_units.get(to)) {
+        let converted = number * from_factor / to_factor;
+        println!("{} {} = {} {}", number, from, converted, to);
+    }
+}
+
+fn weight(number: &f64, from: &str, to: &str) {
+    let mut length_units: HashMap<&str, f64> = HashMap::new();
+    length_units.insert("mg", 0.000001);
+    length_units.insert("g", 0.001);
+    length_units.insert("kg", 1.0);
+    length_units.insert("t", 1000.0);
+    length_units.insert("oz", 0.0283495);
+    length_units.insert("lb", 0.453592);
+    length_units.insert("st", 6.35029);
+    length_units.insert("cwt", 50.8023);
 
     if let (Some(&from_factor), Some(&to_factor)) = (length_units.get(from), length_units.get(to)) {
         let converted = number * from_factor / to_factor;
