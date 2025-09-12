45ed2e8bfa8c7986e2bd24296ad65ba8ba0b2a84
Dest0 <dest.supp@proton.me>
2025-09-12 20:50:20 +0300

    Optimized the code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,63 @@
 use std::collections::HashMap;
 
 fn main() {
+    let length_units: HashMap<&str, f64> = [
+        ("mm", 0.001),
+        ("cm", 0.01),
+        ("in", 0.0254),
+        ("dm", 0.1),
+        ("ft", 0.3048),
+        ("yd", 0.9144),
+        ("m", 1.0),
+        ("km", 1000.0),
+        ("mi", 1609.344),
+    ]
+    .into_iter()
+    .collect();
+
+    let weight_units: HashMap<&str, f64> = [
+        ("mg", 0.000001),
+        ("g", 0.001),
+        ("kg", 1.0),
+        ("t", 1000.0),
+        ("oz", 0.0283495),
+        ("lb", 0.453592),
+        ("st", 6.35029),
+        ("cwt", 50.8023),
+    ]
+    .into_iter()
+    .collect();
+
+    let volume_units: HashMap<&str, f64> =
+        [("ml", 0.000001), ("cl", 0.001), ("dl", 1.0), ("l", 1000.0)]
+            .into_iter()
+            .collect();
+
+    let time_units: HashMap<&str, f64> = [
+        ("ns", 1e-9),
+        ("us", 1e-6),
+        ("ms", 1e-3),
+        ("s", 1.0),
+        ("min", 60.0),
+        ("h", 3600.0),
+        ("d", 86400.0),
+    ]
+    .into_iter()
+    .collect();
+
+    let storage_units: HashMap<&str, f64> = [
+        ("kib", 1024.0),
+        ("mib", 1024.0 * 1024.0),
+        ("gib", 1024.0 * 1024.0 * 1024.0),
+        ("tib", 1099511627776.0),
+        ("b", 1.0),
+        ("kb", 1000.0),
+        ("mb", 1_000_000.0),
+        ("gb", 1_000_000_000.0),
+        ("tb", 1_000_000_000_000.0),
+    ]
+    .into_iter()
+    .collect();
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 4 {
@@ -11,110 +68,74 @@ fn main() {
     let input = &args[1];
 
     if let Ok(num) = input.parse::<f64>() {
-        convert(&num, &args[2], &args[3])
+        convert(
+            &num,
+            &args[2],
+            &args[3],
+            &length_units,
+            &weight_units,
+            &volume_units,
+            &time_units,
+            &storage_units,
+        )
     } else {
         println!("This is not a number");
     }
 }
 
-fn convert(number: &f64, from: &str, to: &str) {
-    let length_list = ["mm", "cm", "in", "dm", "ft", "yd", "m", "km", "mi"];
-    let weight_list = ["mg", "g", "kg", "t", "oz", "lb", "st", "cwt"];
-    let volume_list = ["ml", "cl", "dl", "l"];
-    let time_list = ["ns", "us", "ms", "s", "min", "h", "d"];
-    let storage_list = ["b", "kib", "mib", "gib", "tib", "b", "kb", "mb", "gb", "tb"];
-
-    if length_list.iter().any(|&x| x == from) && length_list.iter().any(|&x| x == to) {
-        length(number, from, to);
-    } else if weight_list.iter().any(|&x| x == from) && weight_list.iter().any(|&x| x == to) {
-        weight(number, from, to);
-    } else if volume_list.iter().any(|&x| x == from) && volume_list.iter().any(|&x| x == to) {
-        volume(number, from, to);
-    } else if time_list.iter().any(|&x| x == from) && time_list.iter().any(|&x| x == to) {
-        time(number, from, to);
-    } else if storage_list.iter().any(|&x| x == from) && storage_list.iter().any(|&x| x == to) {
-        storage(number, from, to);
+fn convert(
+    number: &f64,
+    from: &str,
+    to: &str,
+    length_list: &HashMap<&str, f64>,
+    weight_list: &HashMap<&str, f64>,
+    volume_list: &HashMap<&str, f64>,
+    time_list: &HashMap<&str, f64>,
+    storage_list: &HashMap<&str, f64>,
+) {
+    if length_list.contains_key(from) && length_list.contains_key(to) {
+        length(number, from, to, length_list);
+    } else if weight_list.contains_key(from) && weight_list.contains_key(to) {
+        weight(number, from, to, weight_list);
+    } else if volume_list.contains_key(from) && volume_list.contains_key(to) {
+        volume(number, from, to, time_list);
+    } else if time_list.contains_key(from) && time_list.contains_key(to) {
+        time(number, from, to, time_list);
+    } else if storage_list.contains_key(from) && storage_list.contains_key(to) {
+        storage(number, from, to, storage_list);
     } else {
         println!("Unknown unit")
     }
 }
 
-fn length(number: &f64, from: &str, to: &str) {
-    let mut length_units: HashMap<&str, f64> = HashMap::new();
-    length_units.insert("mm", 0.001);
-    length_units.insert("cm", 0.01);
-    length_units.insert("in", 0.0254);
-    length_units.insert("dm", 0.1);
-    length_units.insert("ft", 0.3048);
-    length_units.insert("yd", 0.9144);
-    length_units.insert("m", 1.0);
-    length_units.insert("km", 1000.0);
-    length_units.insert("mi", 1609.344);
-
-    if let (Some(&from_factor), Some(&to_factor)) = (length_units.get(from), length_units.get(to)) {
+fn length(number: &f64, from: &str, to: &str, length_list: &HashMap<&str, f64>) {
+    if let (Some(&from_factor), Some(&to_factor)) = (length_list.get(from), length_list.get(to)) {
         let converted = number * from_factor / to_factor;
         println!("{} {} = {} {}", number, from, converted, to);
     }
 }
 
-fn weight(number: &f64, from: &str, to: &str) {
-    let mut length_units: HashMap<&str, f64> = HashMap::new();
-    length_units.insert("mg", 0.000001);
-    length_units.insert("g", 0.001);
-    length_units.insert("kg", 1.0);
-    length_units.insert("t", 1000.0);
-    length_units.insert("oz", 0.0283495);
-    length_units.insert("lb", 0.453592);
-    length_units.insert("st", 6.35029);
-    length_units.insert("cwt", 50.8023);
-
-    if let (Some(&from_factor), Some(&to_factor)) = (length_units.get(from), length_units.get(to)) {
+fn weight(number: &f64, from: &str, to: &str, weight_list: &HashMap<&str, f64>) {
+    if let (Some(&from_factor), Some(&to_factor)) = (weight_list.get(from), weight_list.get(to)) {
         let converted = number * from_factor / to_factor;
         println!("{} {} = {} {}", number, from, converted, to);
     }
 }
 
-fn volume(number: &f64, from: &str, to: &str) {
-    let mut length_units: HashMap<&str, f64> = HashMap::new();
-    length_units.insert("ml", 0.000001);
-    length_units.insert("cl", 0.001);
-    length_units.insert("dl", 1.0);
-    length_units.insert("l", 1000.0);
-
-    if let (Some(&from_factor), Some(&to_factor)) = (length_units.get(from), length_units.get(to)) {
+fn volume(number: &f64, from: &str, to: &str, volume_list: &HashMap<&str, f64>) {
+    if let (Some(&from_factor), Some(&to_factor)) = (volume_list.get(from), volume_list.get(to)) {
         let converted = number * from_factor / to_factor;
         println!("{} {} = {} {}", number, from, converted, to);
     }
 }
-fn time(number: &f64, from: &str, to: &str) {
-    let mut length_units: HashMap<&str, f64> = HashMap::new();
-    length_units.insert("ns", 0.000000001);
-    length_units.insert("us", 0.000001);
-    length_units.insert("ms", 0.001);
-    length_units.insert("s", 1.0);
-    length_units.insert("min", 60.0);
-    length_units.insert("h", 3600.0);
-    length_units.insert("d", 86400.0);
-
-    if let (Some(&from_factor), Some(&to_factor)) = (length_units.get(from), length_units.get(to)) {
+fn time(number: &f64, from: &str, to: &str, time_list: &HashMap<&str, f64>) {
+    if let (Some(&from_factor), Some(&to_factor)) = (time_list.get(from), time_list.get(to)) {
         let converted = number * from_factor / to_factor;
         println!("{} {} = {} {}", number, from, converted, to);
     }
 }
-fn storage(number: &f64, from: &str, to: &str) {
-    let mut length_units: HashMap<&str, f64> = HashMap::new();
-    length_units.insert("b", 1.0);
-    length_units.insert("kib", 1024 as f64);
-    length_units.insert("mib", (1024 * 1024) as f64);
-    length_units.insert("gib", (1024 * 1024 * 1024) as f64);
-    length_units.insert("tib", 1099511627776.0);
-    length_units.insert("b", 1.0);
-    length_units.insert("kb", 1000.0);
-    length_units.insert("mb", (1000 * 1000) as f64);
-    length_units.insert("gb", (1000 * 1000 * 1000) as f64);
-    length_units.insert("tb", 1000000000000.0);
-
-    if let (Some(&from_factor), Some(&to_factor)) = (length_units.get(from), length_units.get(to)) {
+fn storage(number: &f64, from: &str, to: &str, storage_list: &HashMap<&str, f64>) {
+    if let (Some(&from_factor), Some(&to_factor)) = (storage_list.get(from), storage_list.get(to)) {
         let converted = number * from_factor / to_factor;
         println!("{} {} = {} {}", number, from, converted, to);
     }
